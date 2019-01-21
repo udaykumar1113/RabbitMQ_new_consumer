331f922452e0817815621918cfa421b45ec4714b
udayk <udaykumar1113@gmail.com>
2019-01-21 12:05:05 -0500

    Created a consumer to read and process JSON messages

---
+++ src/main/java/com/uday/consumer/Employee/Employee.java
@@ -0,0 +1,41 @@
+package com.uday.consumer.Employee;
+
+import java.time.LocalDateTime;
+
+public class Employee {
+    private String employeeId;
+    private String employeeName;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String employeeId, String employeeName) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", employeeName='" + employeeName + '\'' +
+                '}';
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+}
