b194733ee2938d8c18a8aabaf4aee12bbec26eb2
udayk <udaykumar1113@gmail.com>
2019-01-23 18:51:34 -0500

    Added a consumer which would throw exception when size is greater than 9000

---
+++ src/main/java/com/uday/consumer/service/ConsumeExceptionService.java
@@ -0,0 +1,25 @@
+package com.uday.consumer.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uday.consumer.Employee.Picture;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class ConsumeExceptionService {
+
+    ObjectMapper objectMapper=new ObjectMapper();
+
+    @RabbitListener(queues = "q.mypicture.image")
+    public void consumeException(String message) throws IOException {
+        Picture picture=objectMapper.readValue(message, Picture.class);
+        if(picture.getSize()>=9000) {
+                throw new IllegalArgumentException("Picture size too large");
+        }
+        else {
+            System.out.println(message);
+        }
+    }
+}
